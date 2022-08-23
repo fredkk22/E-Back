8cfbb07c0a1da8ef279e2c9e93b8ed78cdd561ba
fredkk22 <freddykwak123@gmail.com>
2022-08-23 18:10:23 -0400

    Edited category route
    There was an error where the get by ID category route didn't have the correct response in the 200 status code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,14 +21,16 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryByID = await Category.findByPk(req.params.id, { include: { model: Product } });
+    const categoryByID = await Category.findByPk(req.params.id, {
+      include: { model: Product }
+    });
 
     if (!categoryByID) {
       res.status(404).json({ message: 'No category found with that id!' });
       return;
     };
 
-    res.status(200).json(driverData);
+    res.status(200).json(categoryByID);
   } catch (err) {
     res.status(400).json(err);
   };
