ff57ca34736bf13e2d385fc76ce230ae8b2abdd0
fredkk22 <freddykwak123@gmail.com>
2022-08-24 14:48:59 -0400

    Used require for sequelize in server.js instead of detailing everything out twice

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
 const express = require('express');
-const Sequelize = require('sequelize');
 const routes = require('./routes');
 // import sequelize connection
-require('dotenv').config();
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-);
+const sequelize = require('./config/connection')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
